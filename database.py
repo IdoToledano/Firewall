1199aaa80c95437cf7970ee1a52d33a663ee2315
IdoToledano <34353169+IdoToledano@users.noreply.github.com>
2018-02-01 06:50:59 +0000

    Add graphics

--- database.py
+++ database.py
@@ -6,26 +6,27 @@ PORT_NUM = 0
 
 
 def execute_command(command, database, cursor):
-        try:
-            cursor.execute(command)
-        except sql.OperationalError as e:
-            return e
-        database.commit()
+    try:
+        cursor.execute(command)
+    except sql.OperationalError as e:
+        return e
+    database.commit()
+
 
 class Database(object):
     def __init__(self, name):
-    	self.database = sql.connect(name)
-    	self.cursor = self.database.cursor()
+        self.database = sql.connect(name)
+        self.cursor = self.database.cursor()
         # Create tables
         sql_command = """CREATE TABLE IP (address char unique);
-    	"""
-    	execute_command(sql_command, self.database, self.cursor)
+        """
+        execute_command(sql_command, self.database, self.cursor)
         sql_command = "CREATE TABLE Ports (number int unique)"
         execute_command(sql_command, self.database, self.cursor)
 
     def add_ip(self, ip):
-    	sql_command = """INSERT OR IGNORE INTO IP (address) values (%s)""" % ip
-    	execute_command(sql_command, self.database, self.cursor)
+        sql_command = """INSERT OR IGNORE INTO IP (address) values (%s)""" % ip
+        execute_command(sql_command, self.database, self.cursor)
 
     def add_port(self, port):
         sql_command = """INSERT OR IGNORE INTO Ports (number) VALUES (%d)""" % port
@@ -39,14 +40,13 @@ class Database(object):
         sql_command = """DELETE FROM Ports WHERE number == (%d)""" % port
         execute_command(sql_command, self.database, self.cursor)
 
-
     def get_ip(self):
-    	sql_command = "SELECT * FROM IP"
-    	r = self.cursor.execute(sql_command)
-    	# organize results
-        ip_list = [] 
-    	for row in r.fetchall():
-    		ip_list.append(row[IP_STRING])
+        sql_command = "SELECT * FROM IP"
+        r = self.cursor.execute(sql_command)
+        # organize results
+        ip_list = []
+        for row in r.fetchall():
+            ip_list.append(row[IP_STRING])
 
         return ip_list
 
@@ -59,12 +59,4 @@ class Database(object):
             port_list.append(row[PORT_NUM])
 
         return port_list
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
 
--- firewall.py
+++ firewall.py
@@ -1,262 +1,58 @@
-# -*- coding: utf-8 -*-
-import iptc
-from socket import gethostbyname
-from database import Database
+from ip_tables import IpTables
+from algorithm import get_suspicous_ips
+import Tkinter as tk
 
+def ban_by_ip(f, ip):
+    ip = ip.get()
+    f.block_ip(ip)
 
-# chains
-INPUT = 0
-OUTPUT = 1
-FORWARD = 2
+def ban_by_port(f, port):
+    port = port.get()
+    f.block_port(port)
 
-
-class Firewall(object):
-
-    @staticmethod
-    def _default_policy(chains):
-        """
-        Changes policy of all chains to drop except
-        for the output chain
-        """
-        chains[INPUT].set_policy("DROP")
-        chains[OUTPUT].set_policy("ACCEPT")
-        chains[FORWARD].set_policy("DROP")
-
-    @staticmethod
-    def _accept_all(chains):
-        """
-        Incase Drop all made you lose your music from youtube.
-        Change all policies to accept all traffic
-        """
-        chains[INPUT].set_policy("ACCEPT")
-        chains[OUTPUT].set_policy("ACCEPT")
-        chains[FORWARD].set_policy("ACCEPT")
-
-    @staticmethod
-    def _allow_loopback(chains):
-        """
-        Adds rule to allow all traffic between self and self
-        """
-        rule = iptc.Rule()
-        rule.in_interface = "lo"
-        target = iptc.Target(rule, "ACCEPT")
-        rule.target = target
-        chains[INPUT].insert_rule(rule)
-
-    @staticmethod
-    def _allow_established(chains):
-        """
-        Adds rule to allow established and related
-        connections.
-        (connections that I created and connections
-         that are created by those)
-        """
-        rule = iptc.Rule()
-        match = rule.create_match('state')
-        match.state = "RELATED,ESTABLISHED"
-        rule.target = iptc.Target(rule, 'ACCEPT')
-        chains[INPUT].insert_rule(rule)
-
-    def __init__(self):
-        # set firewall's table as the Filter table
-        self.table = iptc.Table(iptc.Table.FILTER)
-
-        # create each chain
-        chain_input = iptc.Chain(self.table, "INPUT")
-        chain_output = iptc.Chain(self.table, "OUTPUT")
-        chain_forward = iptc.Chain(self.table, "FORWARD")
-        self.chains = [chain_input, chain_output, chain_forward]
-        # initiate firewall database
-        self.database = Database("firewall.db")
-
-    def __del__(self):
-        self.reset_iptables()
-
-    def get_from_database(self):
-        """
-        Block all ips and ports that are in the database
-        """
-	# initiate from database
-	ip_list = self.database.get_ip()
-	for ip in ip_list:
-		self.block_ip(ip)
-
-	port_list = self.database.get_ports()
-	for port in port_list:
-		self.block_port(port)
-
-    def basic_protections(self):
-        """
-        Sets up basic firewall protections.
-        """
-        Firewall._default_policy(self.chains)
-        Firewall._allow_loopback(self.chains)
-        Firewall._allow_established(self.chains)
-
-
-    def block_ip(self, ip):
-        """
-        Given and Ip, blocks the ip from all traffic (in or out)
-        """
-        rule = iptc.Rule()
-        # in case ip is invalid
-        try:
-        	rule.set_src(ip)
-        except Exception as e:
-        	return 1, e
-
-        rule.create_target("DROP")
-        self.chains[INPUT].insert_rule(rule)
-
-		# add ip address to database
-        self.database.add_ip(ip)
-
-        return 0, ""
-
-    def block_port(self, port):
-        """
-        Given a port, blocks the port from all traffic
-        """
-        for proto in ["tcp", "udp"]:
-            rule = iptc.Rule()
-
-            rule.protocol = proto
-            match = rule.create_match(proto)
-            # in case the port is invalid
-            try:
-                match.sport = str(port)
-            except Exception as e:
-                return 1, e
-
-            rule.create_target("DROP")
-            self.chains[INPUT].insert_rule(rule)
-
-        # add port to database
-        self.database.add_port(int(port))
-
-        return 0, ""
-
-    def block_site(self, site_name):
-        """
-        Given a site name, finds the sites ip
-        and blocks the ip from all traffic
-        """
-        try:
-            ip = gethostbyname(site_name)
-        except Exception as e:
-            return 1, e
-
-        Firewall.block_ip(self, ip)
-
-        return 0, ""
-
-    def unblock_ip(self, ip):
-        """
-        Unblocks the given Ip if it was blocked
-
-        Returns exception and 1 when the rule
-        did not exist in the chain and 0
-        when there wasn't any exception
-        """
-        rule = iptc.Rule()
-        rule.set_src(ip)
-        rule.create_target("DROP")
-
-        # If ip isn't in chain
-        try:
-            self.chains[INPUT].delete_rule(rule)
-        except Exception as e:
-            return 1, e
-
-        # remove from database
-        self.database.remove_ip(ip)
-
-        # return with no errors
-        return 0, ""
-
-    def unblock_port(self, port):
-        """
-        Unblock a port
-
-        Returns exception and 1 when the rule
-        did not exist in the chain and 0
-        when there wasn't any exception
-        """
-        for proto in ["tcp", "udp"]:
-            rule = iptc.Rule()
-
-            rule.protocol = proto
-            match = iptc.Match(rule, proto)
-            # in case the port is invalid
-            try:
-                match.sport = str(port)
-            except Exception as e:
-                return 1, e
-
-            rule.create_target("DROP")
-
-            # if rule isn't in chain
-            try:
-                self.chains[INPUT].delete_rule(rule)
-            except Exception as e:
-                return 1, e
-
-        # remove from database
-        self.database.remove_port(int(port))
-
-        # return with no errors
-        return 0, ""
-
-    def unblock_site(self, site_name):
-        """
-        finds ip of given site and unblocks it if
-        it was in the chain.
-        """
-        # in case the host name is invalid or doesn't exist
-        try:
-            ip = gethostbyname(site_name)
-        except Exception as e:
-            return 1, e
-
-        # unblock the found ip
-        self.unblock_ip(ip)
-        return 0, ""
-
-    def reset_iptables(self):
-        """
-        Resets iptables to their original state.
-        No rules or extra tabels, all packet counters
-        are reset.
-        """
-        # Let everything in
-        Firewall._accept_all(self.chains)
-        # clear all rules
-        self.table.flush()
-        # zero all packet and byte counters
-        self.table.zero_entries("OUTPUT")
-        self.table.zero_entries("INPUT")
-        self.table.zero_entries("FORWARD")
+def auto_sniff(f, time_to_sniff):
+    try:
+        f.basic_protections()
+        while True:
+            ip_to_ban = get_suspicous_ips(time_to_sniff)
+	    for ip in ip_to_ban:
+		print "{} is going to be banned!".format(ip)
+		f.block_ip(ip)
+    finally:
+        f.reset_iptables()
 
 
 def main():
     """
     Add Documentation here
     """
-    done = False
-    f = Firewall()
+    t = IpTables()
     try:
-        f.basic_protections()
-	f.get_from_database()
-        while not done:
-            req = raw_input("Input port ")
-            if req:
-                ok = f.block_port(req)
-                if ok[1]:
-                    print ok[1]
-            if not req:
-                done = True
-    finally:
-        f.reset_iptables()
+	time_to_sniff = float(raw_input("Enter frequency of sniffing (in seconds, default is 3): "))
+    except Exception:
+	time_to_sniff = 3
+
+    master = tk.Tk()
+
+    ip_str = tk.StringVar()
+    port_str = tk.StringVar()
+
+    ip_entry = tk.Entry(master, textvariable=ip_str)
+    ip_button = tk.Button(master, text="Ban IP", command=lambda: ban_by_ip(t, ip_str))
+    ip_entry.pack()
+    ip_button.pack()
+
+    port_entry = tk.Entry(master, textvariable=port_str)
+    port_button = tk.Button(master, text="Ban Port", command=lambda: ban_by_port(t, port_str))
+    port_entry.pack()
+    port_button.pack()
+
+    start_button = tk.Button(master, text="Start", command=lambda: auto_sniff(t, time_to_sniff))
+    start_button.pack()
+
+    master.mainloop()
+
+    
 
 if __name__ == '__main__':
     main()
